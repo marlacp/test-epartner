c25b630c31d9182047e940c336d1989d8e142421
marlacp <macapripe@gmail.com>
2021-06-16 21:18:07 -0500

    comente pruebas

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -3,7 +3,7 @@ import '../../assets/styles/Content.css';
 export const Content = () => {
     return (
         <div className="cont-content">
-            hola soy un contenido chimbita
+            {/* hola soy un contenido chimbita */}
         </div>
     )
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import '../../assets/styles/Footer.css';
 export const Footer = () => {
     return (
         <footer className="cont-footer">
-            esto es un footer
+            {/* esto es un footer */}
         </footer>
     )
 }
